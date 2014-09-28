029ad33312b3019b5f9afad8a74c72900f39c1aa
alexfakhri <alexfakhri@me.com>
2014-09-28 18:48:56 +0100

    working tests to see if numbers are divisible by 3, 5 and 15

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,11 +1,42 @@
-require 'fizzbuzz'
+require './lib/fizzbuzz.rb'
 
-describe Fizzbuzz do
-	
-
-
-
-
+describe 'fizzbuzz' do
 
 	
+	context "Knows that a number is divisible by" do
+		
+		it "3" do
+			expect(is_divisible_by_three?(3)).to eq(true)
+		end
+		it "5" do
+			expect(is_divisible_by_five?(5)).to eq (true)
+		end
+		it "15" do
+			expect(is_divisible_by_fifteen?(15)).to eq (true)
+		end
+	end
+
+
+	context "knows that a number is not divisible by" do
+		
+		it "3" do
+			expect(is_divisible_by_three?(1)).to eq(false)
+		end
+		it "5" do
+			expect(is_divisible_by_five?(1)).to eq(false)
+		end
+		it "15" do
+			expect(is_divisible_by_fifteen?(1)).to eq(false)
+		end
+
+
+
+
+
+	end
+
+
+
+
+
 end
